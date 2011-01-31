134682f62e065922206655887e6f3f4351728f86
izenja <izenja@cba22842-888d-c370-2124-5b3f8ed688c6>
2011-01-31 02:40:44 +0000

    Implemented core audio playback for Android platform

---
+++ src/ca/sandstorm/luminance/audio/AndroidSoundPlayer.java
@@ -0,0 +1,114 @@
+/**
+ * 
+ */
+package ca.sandstorm.luminance.audio;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import ca.sandstorm.luminance.Luminance;
+
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
+
+/**
+ * @author Zenja
+ * Android sound player manager.
+ */
+public class AndroidSoundPlayer implements IAudioDriver
+{
+    /**
+     * Initialize the Android audio player manager.
+     */
+    public AndroidSoundPlayer()
+    {	
+	mSoundPool = new SoundPool(8, AudioManager.STREAM_MUSIC, 0);   
+	if(mSoundPool == null) {
+	    throw new RuntimeException("Audio: failed to create SoundPool");
+	}
+	
+	mSoundMap = new HashMap<String, Integer>();
+	mStreamMap = new HashMap<Integer, Integer>();
+    }
+
+    /**
+     * Load a sound file for future playback.
+     * @param fileName Name of the file, relative to the audio resources directory
+     * @throws IOException 
+     */
+    public void load(String fileName)
+    {
+	// Load the sound
+	int soundDescriptor = mSoundPool.load(fileName, 1);
+	
+	// Put the sound ID in the sound map
+	mSoundMap.put(fileName, soundDescriptor);
+    }
+
+    /**
+     * Play a sound from a file.
+     * Ideally, the sound should be already loaded with a previous load to call ahead of time,
+     * but it will be loaded now if it wasn't before.
+     * @param fileName Name of the file, relative to the audio resources directory
+     * @volume Volume (between 0 and 1) to play sound at.
+     * @return An identifier for the playing stream.
+     */
+    public int play(String fileName, float volume)
+    {
+	// Load sound if it isn't already loaded
+	if (!mSoundMap.containsKey(fileName)) {
+	    load(fileName);
+	}
+	
+	int soundId = mSoundMap.get(fileName);
+	int streamId = mSoundPool.play(soundId, volume, volume, 1, 0, 1.0f);
+	mStreamMap.put(soundId, streamId);
+	
+	return streamId;
+    }
+    
+    /**
+     * Stop a playing stream.
+     * @param stream ID of stream to stop.
+     */
+    public void stop(int stream)
+    {
+	mSoundPool.stop(stream);
+    }
+    
+    /**
+     * Pause all sounds that are currently playing.
+     */
+    public void pauseAll()
+    {
+        for (int stream : mStreamMap.values()) {
+            mSoundPool.pause(stream);
+        }
+    }
+    
+    /**
+     * Resume all paused sounds.
+     */
+    public void resumeAll()
+    {
+        for (int stream : mStreamMap.values()) {
+            mSoundPool.resume(stream);
+        }
+    }
+    
+    /**
+     * Release (free) all loaded sounds.
+     */
+    public void unloadAll()
+    {
+        for (int sound : mSoundMap.values()) {
+            mSoundPool.unload(sound);
+        }
+    }
+    
+    private SoundPool mSoundPool;
+    private HashMap<String, Integer> mSoundMap;
+    private HashMap<Integer, Integer> mStreamMap;
+}

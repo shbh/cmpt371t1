6e166edae3cdc7dfe76c469630b80ecfabd74c5e
shinhalsafar <shinhalsafar@gmail.com>
2011-02-15 04:56:09 +0000

    - generating skybox vertices, index buffer and texture coords - untested draw routine

--- src/ca/sandstorm/luminance/gameobject/Skybox.java
+++ src/ca/sandstorm/luminance/gameobject/Skybox.java
@@ -1,5 +1,7 @@
 package ca.sandstorm.luminance.gameobject;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
 
@@ -9,14 +11,65 @@ import javax.microedition.khronos.opengles.GL10;
 public class Skybox implements IGameObject
 {
     private FloatBuffer _vertexBuffer;
+    private FloatBuffer _texCoordBuffer;
     private ShortBuffer _indexBuffer;
-
-    private int _totalIndices;
 
 
     public Skybox()
     {
+	// define the squares vertices
+	// each line is one side of the cube
+	float squareVertices[] =
+	{
+		-1, -1, 1,		1, -1, 1,		1, 1, 1,		-1, 1, 1,
+		-1, 1, -1,		1, 1, -1,		1, -1, -1,		-1, -1, -1,
+		1, -1, -1,		1, 1, -1,		1, 1, 1,		1, -1, 1,
+		-1, -1, 1,		-1, 1, 1,		-1, 1, -1,		-1, -1, -1,
+		-1, 1, -1,		1, 1, -1,		1, 1, 1,		-1, 1, 1,
+		-1, -1, 1,		1, -1, 1,		1, -1, -1,		-1, -1, -1,
+	};
 
+	// define the texture coordinates
+	// each line is each side of the cube
+	float texCoords[] = {
+		1, 1,		0, 1,		0, 0,		1, 0,
+		0, 0,		1, 0,		1, 1,		0, 1,
+		0, 1,		0, 0,		1, 0,		1, 1,
+		0, 1,		0, 0,		1, 0,		1, 1,
+		0, 1,		1, 1,		1, 0,		0, 0,
+		0, 1,		1, 1,		1, 0,		0, 0,
+	};
+	
+	// define the indices which make up the quads
+	// for now each tuple is a triangle
+	short indices[] =
+	{
+		0,	2,	1,	0,	3,	2,
+		4,	6,	5,	4,	7,	6,
+		8,	10,	9,	8,	11,	10,
+		12,	14,	13,	12,	15,	14,
+		16,	17,	18,	16,	18,	19,
+		20,	21,	22,	20,	22,	23,		
+	};
+	
+	// init the buffers
+	ByteBuffer byteBuf = ByteBuffer.allocateDirect(squareVertices.length * 4);
+	byteBuf.order(ByteOrder.nativeOrder());
+	_vertexBuffer = byteBuf.asFloatBuffer();
+	_vertexBuffer.put(squareVertices);
+	_vertexBuffer.position(0);
+	
+	byteBuf = ByteBuffer.allocateDirect(texCoords.length * 4);
+	byteBuf.order(ByteOrder.nativeOrder());
+	_texCoordBuffer = byteBuf.asFloatBuffer();
+	_texCoordBuffer.put(texCoords);
+	_texCoordBuffer.position(0);
+	
+	byteBuf = ByteBuffer.allocateDirect(indices.length * 2);
+	byteBuf.order(ByteOrder.nativeOrder());
+	_indexBuffer = byteBuf.asShortBuffer();
+	_indexBuffer.put(indices);
+	_indexBuffer.position(0);		
     }
 
 
@@ -28,6 +81,19 @@ public class Skybox implements IGameObject
 
     public void draw(GL10 gl)
     {
+	int index = 0;
+	
+	gl.glFrontFace(GL10.GL_CW);
+	gl.glVertexPointer(3, GL10.GL_FLOAT, 0, _vertexBuffer);
+	gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, _texCoordBuffer);
+	
+	for(int side = 0; side < 6; side++)
+	{
+		//[textures[side] bind];
+	    	_indexBuffer.position(index);
+		gl.glDrawElements(GL10.GL_TRIANGLES, 6, GL10.GL_UNSIGNED_BYTE, _indexBuffer);
 
+		index += 6;
+	}	
     }
 }

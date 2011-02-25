81cbc14a853b150e478be470ee9873ac6c057110
shinhalsafar <shinhalsafar@gmail.com>
2011-02-25 19:14:02 +0000

    - adding empty Light gameobject class

---
+++ src/ca/sandstorm/luminance/gameobject/Light.java
@@ -0,0 +1,56 @@
+package ca.sandstorm.luminance.gameobject;
+
+import javax.vecmath.Vector3f;
+import javax.vecmath.Vector4f;
+
+public class Light implements IGameObject
+{
+
+    @Override
+    public void initialize()
+    {
+	// TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void update()
+    {
+	// TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void destroy()
+    {
+	// TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public Vector3f getPosition()
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+
+    @Override
+    public Vector4f getRotation()
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+
+    @Override
+    public Vector3f getScale()
+    {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}

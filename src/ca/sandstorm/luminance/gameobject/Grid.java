f0aff2fc73b32f1c234e0f1d2bc41a41109f9a5a
shinhalsafar <shinhalsafar@gmail.com>
2011-03-02 18:53:09 +0000

    - formatted grid source to our style - added a function to convert world coordinates to grid based column, row coordinates

--- src/ca/sandstorm/luminance/gameobject/Grid.java
+++ src/ca/sandstorm/luminance/gameobject/Grid.java
@@ -7,6 +7,7 @@ import java.nio.ShortBuffer;
 import java.util.Vector;
 
 import javax.microedition.khronos.opengles.GL10;
+import javax.vecmath.Vector2f;
 import javax.vecmath.Vector3f;
 import javax.vecmath.Vector4f;
 
@@ -16,14 +17,14 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Grid class
+ * 
  * @author halsafar
- *
+ * 
  */
 public class Grid implements IGameObject
 {
-    private static final Logger _logger = LoggerFactory
-    	.getLogger(Grid.class);    
-    
+    private static final Logger _logger = LoggerFactory.getLogger(Grid.class);
+
     // vertex and index buffers
     private FloatBuffer _vertexBuffer;
     private ShortBuffer _indexBuffer;
@@ -34,6 +35,7 @@ public class Grid implements IGameObject
     // temp vectors to avoid mallocs
     private Vector3f _position;
     private Vector3f _tmpCellCenter;
+    private Vector2f _tmpGridPos;
 
     // grid properties
     private int _cols;
@@ -45,61 +47,50 @@ public class Grid implements IGameObject
 
     /**
      * Constructor.
-     * @param rows The number rows in the grid
-     * @param cols The number cols in the grid
-     * @param cellWidth The width of each grid cell
-     * @param cellHeight The height of each grid cell
+     * 
+     * @param rows
+     *            The number rows in the grid
+     * @param cols
+     *            The number cols in the grid
+     * @param cellWidth
+     *            The width of each grid cell
+     * @param cellHeight
+     *            The height of each grid cell
      * @precon rows != 0, cols != 0, cellWidth != 0, cellHeight != 0
      * @precon (rows+1)*cols*2*2 <= 65535
      * @postcon _vertexBuffer != null, _indexBuffer != null, _totalIndices > 0
      */
     public Grid(int rows, int cols, float cellWidth, float cellHeight)
     {
-	_logger.debug("Grid(" + rows + ", " + cols + ", " + cellWidth + ", " + cellHeight + ")");
-	
+	_logger.debug("Grid(" + rows + ", " + cols + ", " + cellWidth + ", " +
+		      cellHeight + ")");
+
 	// calculate vertices
 	float x = 0.0f;
 	float z = 0.0f;
-	//int numVertices = (rows + 1) * (cols + 1) * 3;
+	// int numVertices = (rows + 1) * (cols + 1) * 3;
 	float[] vertices = null;
 	Vector<Float> vVertices = new Vector<Float>();
 
 	int tmpIndex = 0;
-
-	/*
 	for (int i = 0; i < rows + 1; i++) {
 	    for (int j = 0; j < cols + 1; j++) {
-		x = j * cellWidth;
-		z = i * cellHeight;
+		if (i == 0 || j == 0 || j == cols || i == rows) {
+		    x = j * cellWidth;
+		    z = i * cellHeight;
 
-		// set x,y,z
-		vertices[tmpIndex++] = x;
-		vertices[tmpIndex++] = 0.0f;
-		vertices[tmpIndex++] = z;
-	    }
-	}
-	 */
-	for (int i = 0; i < rows + 1; i++) {
-	    for (int j = 0; j < cols + 1; j++) {
-		if (i == 0 || j == 0 || j == cols || i == rows)
-		{
-    			x = j * cellWidth;
-    			z = i * cellHeight;
-    			
-    			vVertices.add(x);
-    			vVertices.add(0.0f);
-    			vVertices.add(z);
+		    vVertices.add(x);
+		    vVertices.add(0.0f);
+		    vVertices.add(z);
 		}
 	    }
 	}
-	
+
 	vertices = new float[vVertices.size()];
-	for (int i = 0; i < vVertices.size(); i++)
-	{
+	for (int i = 0; i < vVertices.size(); i++) {
 	    vertices[i] = vVertices.get(i);
 	}
-	
-	
+
 	ByteBuffer byteBuf = ByteBuffer.allocateDirect(vertices.length * 4);
 	byteBuf.order(ByteOrder.nativeOrder());
 	_vertexBuffer = byteBuf.asFloatBuffer();
@@ -107,64 +98,40 @@ public class Grid implements IGameObject
 	_vertexBuffer.position(0);
 
 	// calculate indices
-	//_totalIndices = (rows + 1) * cols * 2 * 2;
+	// _totalIndices = (rows + 1) * cols * 2 * 2;
 	_totalIndices = ((rows + 1) + (cols + 1)) * 2;
 	short[] indices = new short[_totalIndices];
 
-	/*
-	// horizontal line indices
-	tmpIndex = 0;
-	for (int i = 0; i < rows + 1; i++) {
-	    for (int j = 0; j < cols; j++) {
-		indices[tmpIndex++] = (short) (j + (i * (rows + 1)));
-		indices[tmpIndex++] = (short) ((j + 1) + (i * (rows + 1)));
-	    }
-	}
 
-	// vertical line indices
-	for (int i = 0; i < rows; i++) {
-	    for (int j = 0; j < cols + 1; j++) {
-		indices[tmpIndex++] = (short) (j + (i * (rows + 1)));
-		indices[tmpIndex++] = (short) ((j) + ((i + 1) * (rows + 1)));
-	    }
-	}
-	*/
-	
 	// horizontal line indices
 	short index1 = 0;
-	short index2 = 0;	
+	short index2 = 0;
 	tmpIndex = 0;
 	for (int i = 0; i < rows + 1; i++) {
-	    if (i == 0)
-	    {
+	    if (i == 0) {
 		index1 = 0;
-		index2 = (short)cols;
-	    }
-	    else
-	    {
-		index1 = (short) (i + (i * cols) - ((i-1)*(cols-1)));
-		
-		if (i == cols)
-		{
+		index2 = (short) cols;
+	    } else {
+		index1 = (short) (i + (i * cols) - ((i - 1) * (cols - 1)));
+
+		if (i == cols) {
 		    index2 = (short) ((vertices.length / 3) - 1);
-		}
-		else
-		{
-		    index2 = (short)(index2 + 2);
+		} else {
+		    index2 = (short) (index2 + 2);
 		}
 	    }
-	    
+
 	    indices[tmpIndex++] = index1;
-	    indices[tmpIndex++] = index2;	    
+	    indices[tmpIndex++] = index2;
 	}
 
 	// vertical line indices
 	index1 = 0;
-	index2 = (short)((vertices.length/3) - cols - 1);
-	for (int i = 0; i < cols + 1; i++) {	    	   
+	index2 = (short) ((vertices.length / 3) - cols - 1);
+	for (int i = 0; i < cols + 1; i++) {
 	    indices[tmpIndex++] = (short) (i);
 	    indices[tmpIndex++] = index2++;
-	}	
+	}
 
 	byteBuf = ByteBuffer.allocateDirect(indices.length * 2);
 	byteBuf.order(ByteOrder.nativeOrder());
@@ -183,36 +150,35 @@ public class Grid implements IGameObject
 	_position.set(0, 0, 0);
 
 	_tmpCellCenter = new Vector3f(0, 0, 0);
+	_tmpGridPos = new Vector2f(0, 0);
     }
-    
-    
+
+
     /**
-     * Initialize the object.
-     * Unused.
+     * Initialize the object. Unused.
      */
     @Override
     public void initialize()
     {
 	// TODO Auto-generated method stub
-	
+
     }
 
 
-
     /**
-     * Destroy the object.
-     * Unused.
+     * Destroy the object. Unused.
      */
     @Override
     public void destroy()
     {
 	// TODO Auto-generated method stub
-	
-    }    
+
+    }
 
 
     /**
      * GetColumnCount()
+     * 
      * @return
      */
     public int getColumnCount()
@@ -223,6 +189,7 @@ public class Grid implements IGameObject
 
     /**
      * GetRowCount()
+     * 
      * @return
      */
     public int getRowCount()
@@ -233,6 +200,7 @@ public class Grid implements IGameObject
 
     /**
      * Get the cell width.
+     * 
      * @return
      */
     public float getCellWidth()
@@ -243,6 +211,7 @@ public class Grid implements IGameObject
 
     /**
      * Get the cell height.
+     * 
      * @return
      */
     public float getCellHeight()
@@ -253,6 +222,7 @@ public class Grid implements IGameObject
 
     /**
      * Derives the total grid width
+     * 
      * @return
      */
     public float getTotalWidth()
@@ -263,16 +233,18 @@ public class Grid implements IGameObject
 
     /**
      * Derives the total grid height
+     * 
      * @return
      */
     public float getTotalHeight()
     {
 	return (_cellHeight * getRowCount());
     }
-    
-    
+
+
     /**
      * Get the object's position.
+     * 
      * @return Object position
      */
     @Override
@@ -281,33 +253,38 @@ public class Grid implements IGameObject
 	return _position;
     }
 
+
     /**
-     * Get the object's rotation.
-     * Unused.
+     * Get the object's rotation. Unused.
+     * 
      * @return Object rotation
      */
     @Override
     public Vector4f getRotation()
     {
-        return null;
+	return null;
     }
 
+
     /**
-     * Get the object's scale.
-     * Unused.
+     * Get the object's scale. Unused.
+     * 
      * @return Object scale
      */
     @Override
     public Vector3f getScale()
     {
 	return null;
-    }    
+    }
 
-    
+
     /**
      * Get the center of a cell in world space.
-     * @param row The row index to center on.
-     * @param col The column index to center on.
+     * 
+     * @param row
+     *            The row index to center on.
+     * @param col
+     *            The column index to center on.
      * @return The world position for the center of that particular cell.
      * @precon row >= 0 && row < _rows, col >= 0 && col < _cols
      * @postcon _tmpCellCenter != null
@@ -316,32 +293,57 @@ public class Grid implements IGameObject
     {
 	float xOffset = _cellWidth / 2.0f;
 	float zOffset = _cellHeight / 2.0f;
-	
+
 	float x = xOffset + (col * _cellWidth);
 	float y = 0.0f;
 	float z = zOffset + (row * _cellHeight);
-	
+
 	_tmpCellCenter.set(x, y, z);
-	
+
 	return _tmpCellCenter;
     }
 
-    
+
     /**
-     * Update the object state.
-     * Unused.
+     * Converts a world position into a grid based cell position. This will
+     * return a vector containing the column,row the world position matches.
+     * Note, even though y is accepted as a param it is ignored.
+     * 
+     * @param x
+     *            World x coordinate
+     * @param y
+     *            World y coordinate (ignored)
+     * @param z
+     *            World z coordinate
+     * @return
+     */
+    public Vector2f getGridPosition(float x, float y, float z)
+    {
+	int gridX = (int) (x / _cellWidth);
+	int gridZ = (int) (z / _cellWidth);
+
+	_tmpGridPos.set(gridX, gridZ);
+
+	return _tmpGridPos;
+    }
+
+
+    /**
+     * Update the object state. Unused.
      */
     @Override
     public void update()
     {
 	// TODO Auto-generated method stub
-	
-    }    
+
+    }
 
 
     /**
      * Draw the grid.
-     * @param gl OpenGL context
+     * 
+     * @param gl
+     *            OpenGL context
      */
     public void draw(GL10 gl)
     {
@@ -355,7 +357,7 @@ public class Grid implements IGameObject
 	// Enable the vertex and color state
 	gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
 	// gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
-	
+
 	// Set color
 	gl.glColor4f(1f, 1f, 1f, 1f);
 

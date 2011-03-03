72150d23d3046188bc149937700c6a6f71422066
MeekoTheRogue <MeekoTheRogue@cba22842-888d-c370-2124-5b3f8ed688c6>
2011-03-03 08:36:45 +0000

    Updated other gameobject tests.

--- TestSuite/src/ca/sandstorm/luminance/test/gameobject/BoxTest.java
+++ TestSuite/src/ca/sandstorm/luminance/test/gameobject/BoxTest.java
@@ -19,74 +19,73 @@ public class BoxTest extends AndroidTestCase {
 	 * Create an instance of Box to test. 
 	 */
 	protected void setUp() throws Exception {
-		super.setUp();
-		myBox = new Box(new Vector3f(0.5f, 0.5f, 0.5f) ,new Vector3f(0.5f, 0.5f, 0.5f));
-	}
+	super.setUp();
+	myBox = new Box(new Vector3f(0.5f, 0.5f, 0.5f) ,new Vector3f(0.5f, 0.5f, 0.5f));
+	 }
 	
 	/**
 	 * Test the getRenderable method.
 	 * @throws Exception
 	 */
 	public void testGetRenderable() throws Exception {
-		assertNotNull(myBox.getRenderable());
-	}
+	assertNotNull(myBox.getRenderable());
+	 }
 	
 	/**
 	 * Test the getTexture() method.
 	 * @throws Exception
 	 */
 	public void testGetTexture() throws Exception {
-		assertNotNull(myBox.getTexture());
-	}
+	assertNotNull(myBox.getTexture());
+	 }
 	
 	/**
 	 * Test the getPosition() method.
 	 * @throws Exception
 	 */
 	public void testGetPosition() throws Exception {
-		Vector3f dummy = myBox.getPosition();
-		assertTrue(dummy.x == 0.5f);
-		assertTrue(dummy.y == 0.5f);
-		assertTrue(dummy.z == 0.5f);
-	}
+	Vector3f dummy = myBox.getPosition();
+	assertTrue(dummy.x == 0.5f);
+	assertTrue(dummy.y == 0.5f);
+	assertTrue(dummy.z == 0.5f);
+	 }
 	
 	/**
 	 * Test the getRotation() method.
 	 * @throws Exception
 	 */
 	public void testGetRotation() throws Exception {
-		Vector4f dummy = myBox.getRotation();
-		assertTrue(dummy.w == 1.0f);
-		assertTrue(dummy.x == 1.0f);
-		assertTrue(dummy.y == 1.0f);
-		assertTrue(dummy.z == 1.0f);
-	}
+	Vector4f dummy = myBox.getRotation();
+	assertTrue(dummy.w == 1.0f);
+	assertTrue(dummy.x == 1.0f);
+	assertTrue(dummy.y == 1.0f);
+	assertTrue(dummy.z == 1.0f);
+	 }
 	
 	/**
 	 * Test the getScale() method.
 	 * @throws Exception
 	 */
 	public void testGetScale() throws Exception {
-		Vector3f dummy = myBox.getScale();
-		assertTrue(dummy.x == 0.5f);
-		assertTrue(dummy.y == 0.5f);
-		assertTrue(dummy.z == 0.5f);
-	}
+	Vector3f dummy = myBox.getScale();
+	assertTrue(dummy.x == 0.5f);
+	assertTrue(dummy.y == 0.5f);
+	assertTrue(dummy.z == 0.5f);
+	 }
 	
 	/**
 	 * Test the getRenderType() method.
 	 * @throws Exception
 	 */
 	public void testGetRenderType() throws Exception {
-		RenderType dummy = myBox.getRenderType();
-		assertTrue(dummy == RenderType.Normal);
-	}
+	RenderType dummy = myBox.getRenderType();
+	assertTrue(dummy == RenderType.Normal);
+	 }
 	
 	/**
-	 * Dispose of the created resource
+	 * Dispose of the created resource.
 	 */
-	protected void tearDown() throws Exception
-	{
-		super.tearDown();
-	}
+	protected void tearDown() throws Exception{
+	super.tearDown();
+	 }
 }

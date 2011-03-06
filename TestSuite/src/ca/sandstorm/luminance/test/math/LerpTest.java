33f01459b220ebe0bdc24c3ecc25720857ba3745
cwcollins <cwcollins@gmail.com>
2011-03-06 23:59:47 +0000

    Added LerpTest class

---
+++ TestSuite/src/ca/sandstorm/luminance/test/math/LerpTest.java
@@ -0,0 +1,42 @@
+package ca.sandstorm.luminance.test.math;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Testing of the Lerp class 
+ * @author Chet
+ *
+ */
+public class LerpTest extends AndroidTestCase {
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.test.AndroidTestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	/*
+	 * Testing of the lerp method
+	 */
+	public void testLerp() throws Exception {
+		
+	}
+	
+	/*
+	 * Testing of the ease() method
+	 */
+	public void testEase() throws Exception {
+		
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.test.AndroidTestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}

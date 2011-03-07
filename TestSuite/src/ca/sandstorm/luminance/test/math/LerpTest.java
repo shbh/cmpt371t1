4bc020ab4ce51fdaa1b6eca9d5f36a085b6f204d
cwcollins <cwcollins@gmail.com>
2011-03-07 00:21:47 +0000

    Removed redundant comments from LerpTest

--- TestSuite/src/ca/sandstorm/luminance/test/math/LerpTest.java
+++ TestSuite/src/ca/sandstorm/luminance/test/math/LerpTest.java
@@ -44,28 +44,6 @@ public class LerpTest extends AndroidTestCase {
 	}
 	
 	/*
-	 * public static float ease(float start, float target, float duration,
-	    float timeSinceStart)
-    {
-	float value = start;
-	if (timeSinceStart > 0.0f && timeSinceStart < duration) {
-	    final float range = target - start;
-	    final float percent = timeSinceStart / (duration / 2.0f);
-	    if (percent < 1.0f) {
-		value = start + ((range / 2.0f) * percent * percent * percent);
-	    } else {
-		final float shiftedPercent = percent - 2.0f;
-		value = start +
-			((range / 2.0f) * ((shiftedPercent * shiftedPercent * shiftedPercent) + 2.0f));
-	    }
-	} else if (timeSinceStart >= duration) {
-	    value = target;
-	}
-	return value;
-    }
-	 */
-	
-	/*
 	 * Testing of the ease() method
 	 */
 	public void testEase() throws Exception {

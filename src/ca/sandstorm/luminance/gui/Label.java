e49281ba89566e81da53855ad8142b031868297e
izenja <izenja@cba22842-888d-c370-2124-5b3f8ed688c6>
2011-03-08 02:48:05 +0000

    Changed Label to use PrimitiveQuad for rendering

--- src/ca/sandstorm/luminance/gui/Label.java
+++ src/ca/sandstorm/luminance/gui/Label.java
@@ -1,12 +1,9 @@
 package ca.sandstorm.luminance.gui;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
-
 import javax.microedition.khronos.opengles.GL10;
+import javax.vecmath.Vector3f;
 
-import ca.sandstorm.luminance.graphics.PrimitiveBox;
+import ca.sandstorm.luminance.graphics.PrimitiveQuad;
 import ca.sandstorm.luminance.resources.TextureResource;
 
 /**
@@ -23,10 +20,7 @@ public class Label implements IWidget
     private float _height;
     private String _text;
     
-    // static because all quads only need one of these, just change textures
-    private static FloatBuffer _vertexBuffer;
-    private static float[] _vertices;
-    
+    private PrimitiveQuad _quad;
     private String _textureResourceLocation;
     private TextureResource _texture;
 
@@ -44,23 +38,10 @@ public class Label implements IWidget
 	_height = height;
 	_text = text;
 	
-	// allocate these only once
-	if (_vertices == null || _vertexBuffer == null)
-	{
-	     _vertices = new float[] {
-		         // Vertices for the square
-		          0, height,  0.0f,  // 0. left-bottom
-		          width, height,  0.0f,  // 1. right-bottom
-		          0,  0,  0.0f,  // 2. left-top
-		          width,  0,  0.0f   // 3. right-top
-		};
-        	
-        	ByteBuffer byteBuf = ByteBuffer.allocateDirect(_vertices.length * 4);
-        	byteBuf.order(ByteOrder.nativeOrder());
-        	_vertexBuffer = byteBuf.asFloatBuffer();
-        	_vertexBuffer.put(_vertices);
-        	_vertexBuffer.position(0);
-	}
+	_quad = new PrimitiveQuad(
+	                          new Vector3f(0, 0, 0),
+	                          new Vector3f(width, height, 0)
+	);
     }
     
     /**
@@ -136,16 +117,11 @@ public class Label implements IWidget
     {
 	gl.glPushMatrix();
 	
-	PrimitiveBox box = new PrimitiveBox();
 	gl.glEnable(GL10.GL_TEXTURE_2D);
 	gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 	gl.glBindTexture(GL10.GL_TEXTURE_2D, _texture.getTexture());
-
-	//gl.glScalef(this.width, this.height, 1.0f);
-	gl.glTranslatef(this._x + _width/2, this._y + _height/2, -10);
-	gl.glScalef(100f, 15f, 2f);
-	
-	box.draw(gl);
+	gl.glTranslatef(_x, _y, 0);
+	_quad.draw(gl);	
 	
 	gl.glPopMatrix();
     }

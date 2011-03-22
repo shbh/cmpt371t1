02c266ce16eace39e9bae10e6f798e1408fc9043
shinhalsafar <shinhalsafar@gmail.com>
2011-03-22 23:15:36 +0000

    - enabled fps counter again until demo time

--- src/ca/sandstorm/luminance/Luminance.java
+++ src/ca/sandstorm/luminance/Luminance.java
@@ -280,11 +280,11 @@ public class Luminance extends Activity
 	public void onDrawFrame(GL10 gl)
 	{
 	    // do not log...
-	    /*Message msg = new Message();
+	    Message msg = new Message();
 	    Bundle b = new Bundle();
 	    b.putFloat("fps", Engine.getInstance().getTimer().getFrameDelta());
 	    msg.setData(b);
-	    handler.sendMessage(msg);*/
+	    handler.sendMessage(msg);
 
 	    // update/draw engine
 	    Engine.getInstance().update(gl);

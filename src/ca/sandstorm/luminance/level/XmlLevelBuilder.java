d5beb671f14c9fd2d351d64a5eb21aa03c3ef8e3
steven.indzeoski <steven.indzeoski@gmail.com>
2011-03-26 01:41:50 +0000

    XmlLevelBuilder now completely converts an XmlLevelObject into a DOM Document object.  Doesn't actually output to a file yet though.

--- src/ca/sandstorm/luminance/level/XmlLevelBuilder.java
+++ src/ca/sandstorm/luminance/level/XmlLevelBuilder.java
@@ -8,6 +8,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Text;
 
+import android.graphics.Color;
+
 public class XmlLevelBuilder
 {
     private DocumentBuilder _builder;
@@ -21,57 +23,116 @@ public class XmlLevelBuilder
     
     public Document build(XmlLevel level)
     {
-	// TODO
-	return null;
+	_doc = _builder.newDocument();
+	_doc.appendChild(createLevel(level));
+	return _doc;
     } 
+    
+    public void saveToFile(String filename)
+    {
+	// TODO
+    }
     
     private Element createLevel(XmlLevel level)
     {
-	// TODO
-	return null;
+	Element e = _doc.createElement("level");
+	e.appendChild(createTextElement("name", level.getName()));
+	e.appendChild(createTextElement("difficulty", level.getDifficulty()));
+	e.appendChild(createGridSize(level.getXSize(), level.getYSize(), level.getWidth(), level.getHeight()));
+	for (XmlLevelObject levelObject : level.getObjects())
+	{
+	    e.appendChild(createObject(levelObject));
+	}
+	
+	for (XmlLevelTool levelTool : level.getTools())
+	{
+	    e.appendChild(createTool(levelTool));
+	}
+	return e;
     }
     
-    private Element createBrick(XmlLevelBrick brick)
+    private Element createGridSize(int sizeX, int sizeY, float width, float height)
     {
-	// TODO
-	return null;
-    }
-    
-    private Element createEmitter(XmlLevelEmitter emitter)
-    {
-	// TODO
-	return null;
-    }
-    
-    private Element createGoal(XmlLevelGoal goal)
-    {
-	// TODO
-	return null;
-    }
-    
-    private Element createMirror(XmlLevelMirror mirror)
-    {
-	// TODO
-	return null;
+	Element e = _doc.createElement("grid_size");
+	e.appendChild(createTextElement("x", String.valueOf(sizeX)));
+	e.appendChild(createTextElement("y", String.valueOf(sizeY)));
+	e.appendChild(createTextElement("width", String.valueOf(width)));
+	e.appendChild(createTextElement("height", String.valueOf(height)));
+	return e;
     }
     
     private Element createObject(XmlLevelObject object)
     {
 	Element e = _doc.createElement("object");
-	// TODO
-	return null;
+	e.appendChild(createTextElement("type", object.getType()));
+	if (object.getType().equals(XmlLevelGoal.getId()))
+	{
+	    switch (((XmlLevelGoal)object).getColour())
+	    {
+		case Color.WHITE:
+		    e.appendChild(createTextElement("colour", "white"));
+		    break;
+		case Color.RED:
+		    e.appendChild(createTextElement("colour", "red"));
+		    break;
+		case Color.GREEN:
+		    e.appendChild(createTextElement("colour", "green"));
+		    break;
+		case Color.BLUE:
+		    e.appendChild(createTextElement("colour", "blue"));
+		    break;
+		default:
+		    throw new IllegalArgumentException("Goal has an invalid colour.");
+	    }
+	}
+	else if (object.getType().equals(XmlLevelEmitter.getId()))
+	{
+	    switch (((XmlLevelEmitter)object).getColour())
+	    {
+		case Color.WHITE:
+		    e.appendChild(createTextElement("colour", "white"));
+		    break;
+		case Color.RED:
+		    e.appendChild(createTextElement("colour", "red"));
+		    break;
+		case Color.GREEN:
+		    e.appendChild(createTextElement("colour", "green"));
+		    break;
+		case Color.BLUE:
+		    e.appendChild(createTextElement("colour", "blue"));
+		    break;
+		default:
+		    throw new IllegalArgumentException("Emitter has an invalid colour.");
+	    }
+	}
+	e.appendChild(createObjectPosition(object.getPositionX(), object.getPositionY()));
+	e.appendChild(createObjectRotation(object.getRotationX(), object.getRotationY(), object.getRotationZ()));
+	return e;
     }
     
-    private Element createPrism(XmlLevelPrism prism)
+    private Element createObjectPosition(float positionX, float positionY)
     {
-	// TODO
-	return null;
+	Element e = _doc.createElement("position");
+	e.appendChild(createTextElement("x", String.valueOf(positionX)));
+	e.appendChild(createTextElement("y", String.valueOf(positionY)));
+	return e;
+    }
+    
+    private Element createObjectRotation(float rotationX, float rotationY, float rotationZ)
+    {
+	Element e = _doc.createElement("rotation");
+	e.appendChild(createTextElement("x", String.valueOf(rotationX)));
+	e.appendChild(createTextElement("y", String.valueOf(rotationY)));
+	e.appendChild(createTextElement("z", String.valueOf(rotationZ)));
+	return e;
     }
     
     private Element createTool(XmlLevelTool tool)
     {
-	// TODO
-	return null;
+	Element e = _doc.createElement("tool");
+	e.appendChild(createTextElement("type", tool.getType()));
+	e.appendChild(createTextElement("count", String.valueOf(tool.getCount())));
+	return e;
     }
 
     private Element createTextElement(String name, String text)

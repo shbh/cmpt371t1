15db0127f8b7b871c4fd61031407663d19236985
steven.indzeoski <steven.indzeoski@gmail.com>
2011-04-04 19:38:40 +0000

    Did major refactoring to XmlLevelParser.  Split the one giant method into many helper methods.  This helps a lot in readability.  I still need to add JavaDoc comments though.

--- src/ca/sandstorm/luminance/level/XmlLevelParser.java
+++ src/ca/sandstorm/luminance/level/XmlLevelParser.java
@@ -1,14 +1,18 @@
 package ca.sandstorm.luminance.level;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.Vector;
 
-import javax.xml.parsers.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 
 /**
@@ -43,195 +47,319 @@ public class XmlLevelParser
      * @precond XmlLevelParser was successfully created
      * @postcond An XmlLevel is created from information parsed from the XML file
      * @return An XmlLevel object that contains all information about the level.
+     * @throws IOException  
      */
-    public XmlLevel parse()
-    {	    
+    public XmlLevel parse() throws IOException
+    {	
 	try {
-	    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-	    DocumentBuilder db = dbf.newDocumentBuilder();
-	    Document doc = db.parse(_file);
-	    doc.getDocumentElement().normalize();
+	    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		Document document = documentBuilder.parse(_file);
+		document.getDocumentElement().normalize();
 
-	    // Check that root element is "level"
-	    String rootElement = doc.getDocumentElement().getNodeName();
-	    if (!rootElement.equals("level")) {
-		throw new Exception("The XML file is not a level.");
-	    }
-
-	    // Get the level name.
-	    Element nameElement = (Element)doc.getElementsByTagName("name").item(0);
-	    String name = ((Node)nameElement.getChildNodes().item(0)).getNodeValue();
-
-	    // Get the difficulty.
-	    Element difficultyElement = (Element)doc.getElementsByTagName("difficulty").item(0);
-	    String difficulty = ((Node)difficultyElement.getChildNodes().item(0)).getNodeValue();
-
-	    // Get the dimensions of the level.
-	    Element gridElement = (Element)doc.getElementsByTagName("grid_size").item(0);
-	    Element xElement = (Element)gridElement.getElementsByTagName("x").item(0);
-	    String x = ((Node)xElement.getChildNodes().item(0)).getNodeValue();
-	    int xString = Integer.parseInt(x);
-	    Element yElement = (Element)gridElement.getElementsByTagName("y").item(0);
-	    String y = ((Node)yElement.getChildNodes().item(0)).getNodeValue();
-	    int yString = Integer.parseInt(y);
-
-	    Element wElement = (Element)gridElement.getElementsByTagName("width").item(0);
-	    String w = ((Node)wElement.getChildNodes().item(0)).getNodeValue();
-	    float fW = Float.parseFloat(w);
-
-	    Element hElement = (Element)gridElement.getElementsByTagName("height").item(0);
-	    String h = ((Node)hElement.getChildNodes().item(0)).getNodeValue();
-	    float fH = Float.parseFloat(h);		
-
-
-	    // Get level objects.
-	    LinkedList<XmlLevelObject> objectList = new LinkedList<XmlLevelObject>();
-	    NodeList objectNodeList = doc.getElementsByTagName("object");
-	    for (int i = 0; i < objectNodeList.getLength(); i++)  {
-		Node node = objectNodeList.item(i);
-
-		if (node.getNodeType() == Node.ELEMENT_NODE) {
-		    Element element = (Element)node;
-
-		    // Get the type
-		    NodeList typeNodeList = element.getElementsByTagName("type");
-		    Element typeElement = (Element)typeNodeList.item(0);
-		    NodeList type = typeElement.getChildNodes();
-		    String typeString = ((Node)type.item(0)).getNodeValue();
-
-		    // Get the colour if object is a goal or emitter
-		    String colourString = null;
-		    if (typeString.equals(XmlLevelGoal.getId()) || typeString.equals(XmlLevelEmitter.getId())) {
-			NodeList colourNodeList = element.getElementsByTagName("colour");
-			Element colourElement = (Element)colourNodeList.item(0);
-			NodeList colour = colourElement.getChildNodes();
-			colourString = ((Node)colour.item(0)).getNodeValue();
-			//System.out.println("Colour: " + colourString);
-		    }
-
-		    // Get the position
-		    NodeList positionNodeList = element.getElementsByTagName("position");
-		    Element positionElement = (Element)positionNodeList.item(0);
-
-		    // Get the x position
-		    NodeList objectXNodeList = positionElement.getElementsByTagName("x");
-		    Element objectXElement = (Element)objectXNodeList.item(0);
-		    NodeList objectX = objectXElement.getChildNodes();
-		    String objectXString = ((Node)objectX.item(0)).getNodeValue();
-		    //System.out.println("Object x: " + objectXString);
-		    float objectXFloat = 0;
-		    try {
-			objectXFloat = Float.valueOf(objectXString.trim()).floatValue();
-		    } catch (NumberFormatException e) {
-			e.printStackTrace();
-		    }
-
-		    // Get the y position
-		    NodeList objectYNodeList = positionElement.getElementsByTagName("y");
-		    Element objectYElement = (Element)objectYNodeList.item(0);
-		    NodeList objectY = objectYElement.getChildNodes();
-		    String objectYString = ((Node)objectY.item(0)).getNodeValue();
-		    float objectYFloat = 0;
-		    try {
-			objectYFloat = Float.valueOf(objectYString.trim()).floatValue();
-		    } catch (NumberFormatException e) {
-			e.printStackTrace();
-		    }
-		    
-		    // Make the position vector.
-		    Vector<Float> position = new Vector<Float>(2);
-		    position.add(objectXFloat);
-		    position.add(objectYFloat);
-
-		    // Get the rotation
-		    NodeList rotationNodeList = element.getElementsByTagName("rotation");
-		    Element rotationElement = (Element)rotationNodeList.item(0);
-
-		    // Get the x rotation
-		    NodeList objectXRotationNodeList = rotationElement.getElementsByTagName("x");
-		    Element objectXRotationElement = (Element)objectXRotationNodeList.item(0);
-		    NodeList objectXRotation = objectXRotationElement.getChildNodes();
-		    String objectXRotationString = ((Node)objectXRotation.item(0)).getNodeValue();
-		    float objectXRotationFloat = Float.parseFloat(objectXRotationString);
-
-		    // Get the y rotation
-		    NodeList objectYRotationNodeList = rotationElement.getElementsByTagName("y");
-		    Element objectYRotationElement = (Element)objectYRotationNodeList.item(0);
-		    NodeList objectYRotation = objectYRotationElement.getChildNodes();
-		    String objectYRotationString = ((Node)objectYRotation.item(0)).getNodeValue();
-		    float objectYRotationFloat = Float.parseFloat(objectYRotationString);
-
-		    // Get the z rotation
-		    NodeList objectZRotationNodeList = rotationElement.getElementsByTagName("z");
-		    Element objectZRotationElement = (Element)objectZRotationNodeList.item(0);
-		    NodeList objectZRotation = objectZRotationElement.getChildNodes();
-		    String objectZRotationString = ((Node)objectZRotation.item(0)).getNodeValue();
-		    float objectZRotationFloat = Float.parseFloat(objectZRotationString);
-		    
-		    // Create the rotation vector.
-		    Vector<Float> rotation = new Vector<Float>(3);
-		    rotation.add(objectXRotationFloat);
-		    rotation.add(objectYRotationFloat);
-		    rotation.add(objectZRotationFloat);
-
-		    // Create the object for the level
-		    XmlLevelObject xmlLevelObject = null;
-		    if (typeString.equals(XmlLevelBrick.getId())) {
-			xmlLevelObject = new XmlLevelBrick(position, rotation);
-		    }
-		    else if (typeString.equals(XmlLevelGoal.getId())) {
-			xmlLevelObject = new XmlLevelGoal(colourString, position, rotation);
-		    }
-		    else if (typeString.equals(XmlLevelEmitter.getId())) {
-			xmlLevelObject = new XmlLevelEmitter(colourString, position, rotation);
-		    }
-
-		    // Add the object to a linked list of objects
-		    objectList.add(xmlLevelObject);
-		}
-	    } // end of objects for loop
-
-	    // Get tools
-	    NodeList toolNodeList = doc.getElementsByTagName("tool");
-	    LinkedList<XmlLevelTool> toolList = new LinkedList<XmlLevelTool>();
-	    for (int i = 0; i < toolNodeList.getLength(); i++) {
-		Node node = toolNodeList.item(i);
-
-		if (node.getNodeType() == Node.ELEMENT_NODE) {
-		    Element element = (Element)node;
-
-		    // Get the type
-		    NodeList typeNodeList = element.getElementsByTagName("type");
-		    Element typeElement = (Element)typeNodeList.item(0);
-		    NodeList type = typeElement.getChildNodes();
-		    String typeString = ((Node)type.item(0)).getNodeValue();
-
-		    // Get the count
-		    NodeList countNodeList = element.getElementsByTagName("count");
-		    Element countElement = (Element)countNodeList.item(0);
-		    NodeList count = countElement.getChildNodes();
-		    String countString = ((Node)count.item(0)).getNodeValue();
-		    int countInt = Integer.parseInt(countString);
-
-		    // Create the XmlLevelTool and add to list.
-		    XmlLevelTool xmlLevelTool = null;
-		    if (typeString.equals(XmlLevelMirror.getId())) {
-			xmlLevelTool = new XmlLevelMirror(countInt);
-		    }
-		    else if (typeString.equals(XmlLevelPrism.getId())) {
-			xmlLevelTool = new XmlLevelPrism(countInt);
-		    }
-		    toolList.add(xmlLevelTool);
-		}
-	    }
-
-	    // Create and return level object.
-	    XmlLevel xmlLevel = new XmlLevel(name, difficulty, xString, yString, fW, fH, objectList, toolList);	
-	    return xmlLevel;
+		String levelName = getLevelName(document);
+		String levelDifficulty = getLevelDifficulty(document);
+		int levelXSize = getLevelXSize(document);
+		int levelYSize = getLevelYSize(document);
+		float levelWidth = getLevelWidth(document);
+		float levelHeight = getLevelHeight(document);
+		LinkedList<XmlLevelObject> levelObjects = getLevelObjects(document);
+		LinkedList<XmlLevelTool> levelTools = getLevelTools(document);
+		return new XmlLevel(levelName, levelDifficulty, levelXSize, levelYSize, 
+		                    levelWidth, levelHeight, levelObjects, levelTools);
+	} catch (Exception e) {
+	    throw new IOException(e.getMessage());
 	}
-	catch (Exception e)  {
-	    e.printStackTrace();
+    }
+
+
+
+    private LinkedList<XmlLevelTool> getLevelTools(Document document)
+    {
+	LinkedList<XmlLevelTool> toolList = new LinkedList<XmlLevelTool>();
+	NodeList toolNodeList = document.getElementsByTagName("tool");
+	for (int i = 0; i < toolNodeList.getLength(); i++) {
+	    Node toolNode = toolNodeList.item(i);
+	    XmlLevelTool xmlLevelTool = getLevelTool(toolNode);
+	    toolList.add(xmlLevelTool);
 	}
-	return null;
+	return toolList;
+    }
+
+
+
+    private XmlLevelTool getLevelTool(Node toolNode)
+    {
+	if (toolNode.getNodeType() == Node.ELEMENT_NODE) {
+	    Element element = (Element)toolNode;
+	    String toolType = getToolType(element);
+	    int toolCount = getToolCount(element);	
+
+	    // Create the XmlLevelTool and add to list.
+	    XmlLevelTool xmlLevelTool = null;
+	    if (toolType.equals(XmlLevelMirror.getId())) {
+		xmlLevelTool = new XmlLevelMirror(toolCount);
+	    }
+	    else if (toolType.equals(XmlLevelPrism.getId())) {
+		xmlLevelTool = new XmlLevelPrism(toolCount);
+	    }
+	    else {
+		throw new RuntimeException("Level file parsing failed. Invalid tool type \"" + toolType + "\"");
+	    }
+	    return xmlLevelTool;
+	}
+	else {
+	    throw new RuntimeException("Something was wrong with the XML file.");
+	}
+    }
+
+
+
+    private int getToolCount(Element element)
+    {
+	NodeList countNodeList = element.getElementsByTagName("count");
+	Element countElement = (Element)countNodeList.item(0);
+	NodeList count = countElement.getChildNodes();
+	String countString = ((Node)count.item(0)).getNodeValue();
+	return Integer.parseInt(countString);
+    }
+
+
+
+    private String getToolType(Element element)
+    {
+	NodeList typeNodeList = element.getElementsByTagName("type");
+	Element typeElement = (Element)typeNodeList.item(0);
+	NodeList type = typeElement.getChildNodes();
+	return ((Node)type.item(0)).getNodeValue();
+    }
+
+
+
+    private LinkedList<XmlLevelObject> getLevelObjects(Document document)
+    {
+	LinkedList<XmlLevelObject> objectList = new LinkedList<XmlLevelObject>();
+	NodeList objectNodeList = document.getElementsByTagName("object");
+	for (int i = 0; i < objectNodeList.getLength(); i++)  {
+	    Node objectNode = objectNodeList.item(i);
+	    XmlLevelObject xmlLevelObject = getLevelObject(objectNode);
+	    objectList.add(xmlLevelObject);
+	}
+	return objectList;
+    }
+
+
+    private XmlLevelObject getLevelObject(Node objectNode)
+    {
+	if (objectNode.getNodeType() == Node.ELEMENT_NODE) {
+	    Element objectElement = (Element)objectNode;
+	    String objectType = getObjectType(objectElement);
+	    String objectColour = getObjectColour(objectType, objectElement);
+	    Vector<Float> objectPosition = getObjectPosition(objectElement);
+	    Vector<Float> objectRotation = getObjectRotation(objectElement);
+
+	    // Create the object for the level
+	    XmlLevelObject xmlLevelObject = null;
+	    if (objectType.equals(XmlLevelBrick.getId())) {
+		xmlLevelObject = new XmlLevelBrick(objectPosition, objectRotation);
+	    }
+	    else if (objectType.equals(XmlLevelGoal.getId())) {
+		xmlLevelObject = new XmlLevelGoal(objectColour, objectPosition, objectRotation);
+	    }
+	    else if (objectType.equals(XmlLevelEmitter.getId())) {
+		xmlLevelObject = new XmlLevelEmitter(objectColour, objectPosition, objectRotation);
+	    }
+	    else {
+		throw new RuntimeException("Level file parsing failed. Invalid type: \"" + objectType + "\"");
+	    }
+	    return xmlLevelObject;
+	}
+	else {
+	    throw new RuntimeException("Something was wrong with the XML file.");
+	}
+    }
+
+
+
+    private Vector<Float> getObjectRotation(Element element)
+    {
+	Element rotationElement = getRotationElement(element);
+	Vector<Float> rotation = new Vector<Float>(3);
+	rotation.add(getObjectXRotation(rotationElement));
+	rotation.add(getObjectYRotation(rotationElement));
+	rotation.add(getObjectZRotation(rotationElement));
+	return rotation;
+    }
+
+
+
+    private Float getObjectZRotation(Element rotationElement)
+    {
+	NodeList objectZRotationNodeList = rotationElement.getElementsByTagName("z");
+	Element objectZRotationElement = (Element)objectZRotationNodeList.item(0);
+	NodeList objectZRotation = objectZRotationElement.getChildNodes();
+	String objectZRotationString = ((Node)objectZRotation.item(0)).getNodeValue();
+	return Float.parseFloat(objectZRotationString);
+    }
+
+
+
+    private Float getObjectYRotation(Element rotationElement)
+    {
+	NodeList objectYRotationNodeList = rotationElement.getElementsByTagName("y");
+	Element objectYRotationElement = (Element)objectYRotationNodeList.item(0);
+	NodeList objectYRotation = objectYRotationElement.getChildNodes();
+	String objectYRotationString = ((Node)objectYRotation.item(0)).getNodeValue();
+	return Float.parseFloat(objectYRotationString);
+    }
+
+
+
+    private Float getObjectXRotation(Element rotationElement)
+    {
+	NodeList objectXRotationNodeList = rotationElement.getElementsByTagName("x");
+	Element objectXRotationElement = (Element)objectXRotationNodeList.item(0);
+	NodeList objectXRotation = objectXRotationElement.getChildNodes();
+	String objectXRotationString = ((Node)objectXRotation.item(0)).getNodeValue();
+	return Float.parseFloat(objectXRotationString);
+    }
+
+
+
+    private Element getRotationElement(Element element)
+    {
+	NodeList rotationNodeList = element.getElementsByTagName("rotation");
+	return (Element)rotationNodeList.item(0);
+    }
+
+
+
+    private Vector<Float> getObjectPosition(Element element)
+    {
+	Element positionElement = getPositionElement(element);
+	Vector<Float> position = new Vector<Float>(2);
+	position.add(getObjectXPosition(positionElement));
+	position.add(getObjectYPosition(positionElement));
+	return position;
+    }
+
+
+
+    private float getObjectYPosition(Element positionElement)
+    {
+	NodeList objectYNodeList = positionElement.getElementsByTagName("y");
+	Element objectYElement = (Element)objectYNodeList.item(0);
+	NodeList objectY = objectYElement.getChildNodes();
+	String objectYString = ((Node)objectY.item(0)).getNodeValue();
+	return Float.parseFloat(objectYString);
+    }
+
+
+
+    private float getObjectXPosition(Element positionElement)
+    {
+	NodeList objectXNodeList = positionElement.getElementsByTagName("x");
+	Element objectXElement = (Element)objectXNodeList.item(0);
+	NodeList objectX = objectXElement.getChildNodes();
+	String objectXString = ((Node)objectX.item(0)).getNodeValue();
+	return Float.parseFloat(objectXString);
+    }
+
+
+
+    private Element getPositionElement(Element element)
+    {
+	NodeList positionNodeList = element.getElementsByTagName("position");
+	return (Element)positionNodeList.item(0);
+    }
+
+
+
+    private String getObjectColour(String objectType, Element element)
+    {
+	if (objectType.equals(XmlLevelBrick.getId())) {
+	    return "white";
+	}
+	else {
+	    NodeList colourNodeList = element.getElementsByTagName("colour");
+	    Element colourElement = (Element)colourNodeList.item(0);
+	    NodeList colour = colourElement.getChildNodes();
+	    return ((Node)colour.item(0)).getNodeValue();
+	}
+    }
+
+
+
+    private String getObjectType(Element element)
+    {
+	NodeList typeNodeList = element.getElementsByTagName("type");
+	Element typeElement = (Element)typeNodeList.item(0);
+	NodeList type = typeElement.getChildNodes();
+	return ((Node)type.item(0)).getNodeValue();
+    }
+
+
+
+    private float getLevelHeight(Document document)
+    {
+	Element gridSizeElement = getGridSizeElement(document);
+	Element hElement = (Element)gridSizeElement.getElementsByTagName("height").item(0);
+	String h = ((Node)hElement.getChildNodes().item(0)).getNodeValue();
+	return Float.parseFloat(h);
+    }
+
+
+
+    private float getLevelWidth(Document document)
+    {
+	Element gridSizeElement = getGridSizeElement(document);
+	Element wElement = (Element)gridSizeElement.getElementsByTagName("width").item(0);
+	String w = ((Node)wElement.getChildNodes().item(0)).getNodeValue();
+	return Float.parseFloat(w);
+    }
+
+
+
+    private int getLevelYSize(Document document)
+    {
+	Element gridSizeElement = getGridSizeElement(document);
+	Element yElement = (Element)gridSizeElement.getElementsByTagName("y").item(0);
+	String y = ((Node)yElement.getChildNodes().item(0)).getNodeValue();
+	return Integer.parseInt(y);
+    }
+
+
+
+    private int getLevelXSize(Document document)
+    {
+	Element gridSizeElement = getGridSizeElement(document);
+	Element xElement = (Element)gridSizeElement.getElementsByTagName("x").item(0);
+	String x = ((Node)xElement.getChildNodes().item(0)).getNodeValue();
+	return Integer.parseInt(x);
+    }
+
+
+
+    private Element getGridSizeElement(Document document)
+    {
+	return (Element)document.getElementsByTagName("grid_size").item(0);
+    }
+
+
+
+    private String getLevelDifficulty(Document document)
+    {
+	Element difficultyElement = (Element)document.getElementsByTagName("difficulty").item(0);
+	String difficulty = ((Node)difficultyElement.getChildNodes().item(0)).getNodeValue();
+	return difficulty;
+    }
+
+
+
+    private String getLevelName(Document document)
+    {
+	Element nameElement = (Element)document.getElementsByTagName("name").item(0);
+	String name = ((Node)nameElement.getChildNodes().item(0)).getNodeValue();
+	return name;
     }
 }
